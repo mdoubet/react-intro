88f9ceafef798b42dd4f28fab969aad9d25c94f0
Matthew Doubet <matthewdoubet@Matthews-MacBook-Air.local>
2018-03-04 19:56:39 -0700

    add timetravel functionality with jumpTo function

--- src/index.js
+++ src/index.js
@@ -62,13 +62,21 @@ class Game extends React.Component {
                     squares: Array (9).fill(null),
                 }
             ],
+            stepNumber: 0,
             xIsNext: true
         };
     }
 
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
     render() {
         const history = this.state.history;
-        const current = history[history.length-1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -108,8 +116,9 @@ class Game extends React.Component {
             </div>
         );
     }
+
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0,this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -120,6 +129,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
